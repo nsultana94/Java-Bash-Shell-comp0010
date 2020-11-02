bef51a48b1cdc9f87d4c805302d100fa8843f81b
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2020-11-02 14:54:24 +0000

    cat, echo, grep, head, tail creation

---
+++ src/main/java/uk/ac/ucl/jsh/tail.java
@@ -0,0 +1,9 @@
+package uk.ac.ucl.jsh;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+public interface Application {
+    public void exec(List<String> args, String input, OutputStreamWriter output) throws IOException;
+}
