76a08c64e38b31225025ee5db27c84efd28a63a3
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2020-11-02 14:48:33 +0000

    command seq exec to eval

--- src/main/java/uk/ac/ucl/jsh/Cd.java
+++ src/main/java/uk/ac/ucl/jsh/Cd.java
@@ -6,8 +6,6 @@ import java.io.OutputStreamWriter;
 import java.util.List;
 
 public class Cd implements Application {
-
-
     @Override
     public void exec(List<String> args, String input, OutputStreamWriter output) throws IOException {
         // TODO Auto-generated method stub
