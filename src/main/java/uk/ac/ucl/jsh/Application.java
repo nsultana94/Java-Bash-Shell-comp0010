17c216f19830e4d2940f19cc76210f22dbbd2bf3
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2020-11-02 14:31:50 +0000

    Changed Application

--- src/main/java/uk/ac/ucl/jsh/Application.java
+++ src/main/java/uk/ac/ucl/jsh/Application.java
@@ -1,7 +1,9 @@
 package uk.ac.ucl.jsh;
 
-import java.io.OutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
 
 public interface Application {
-    public void exec(String args, String input, OutputStream output);
+    public void exec(List<String> args, String input, OutputStreamWriter output) throws IOException;
 }
