cfd14ec4c511a908710c569284b6e84818b93dd4
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2020-11-16 12:16:20 +0000

    find corrections (line 40 was outside the else when it should have been inside)

--- src/main/java/uk/ac/ucl/jsh/find.java
+++ src/main/java/uk/ac/ucl/jsh/find.java
@@ -19,7 +19,7 @@ public class find implements Application {
             throw new RuntimeException("find: missing arguments");
         } else if (!(args.size() == 3 || args.size() == 2)) {
             throw new RuntimeException("find: wrong arguments");
-        } else if ((args.size() == 3 && !args.get(0).equals("-name"))) {
+        } else if ((args.size() == 2 && !args.get(0).equals("-name"))) {
             throw new RuntimeException("find: wrong arguments " + args.get(0));
         } else if ((args.size() == 3 && !args.get(1).equals("-name"))) {
             throw new RuntimeException("find: wrong arguments " + args.get(1));
@@ -37,8 +37,9 @@ public class find implements Application {
             if (!targetDirectory.exists()) {
                 throw new RuntimeException("find: path does not exist " + args.get(0));
             }
+            patternArg = args.get(2);
         }
-        patternArg = args.get(2);
+
         File[] targetDirListing = targetDirectory.listFiles();
         if (targetDirListing != null) {
             for (File child : targetDirListing) {
