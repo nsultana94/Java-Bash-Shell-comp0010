4946c80a472041abdaff07109e5191fd60656ad4
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2020-11-21 18:52:08 +0000

    tail with Stdin

--- src/main/java/uk/ac/ucl/jsh/tail.java
+++ src/main/java/uk/ac/ucl/jsh/tail.java
@@ -20,15 +20,17 @@ public class tail implements Application {
     
     public void exec(List<String> args, BufferedReader input, OutputStreamWriter output) throws IOException{
         String currentDirectory = directory.getCurrentDirectory();
-        if (args.isEmpty()) {
-            throw new RuntimeException("tail: missing arguments");
-        }
-        if (args.size() != 1 && args.size() != 3) {
+        if (args.size() > 3) {
             throw new RuntimeException("tail: wrong arguments");
         }
         if (args.size() == 3 && !args.get(0).equals("-n")) {
             throw new RuntimeException("tail: wrong argument " + args.get(0));
         }
+        if (args.size() == 2 && !args.get(0).equals("-n")) {
+            throw new RuntimeException("tail: wrong argument " + args.get(0));
+        }
+
+        
         int tailLines = 10;
         String tailArg;
         if (args.size() == 3) {
@@ -40,32 +42,61 @@ public class tail implements Application {
             tailArg = args.get(2);
         } else {
             tailArg = args.get(0);
-        }
-        File tailFile = new File(currentDirectory + File.separator + tailArg);
-        if (tailFile.exists()) {
-            Charset encoding = StandardCharsets.UTF_8;
-            Path filePath = Paths.get((String) currentDirectory + File.separator + tailArg);
-            ArrayList<String> storage = new ArrayList<>();
-            try (BufferedReader reader = Files.newBufferedReader(filePath, encoding)) {
-                String line = null;
-                while ((line = reader.readLine()) != null) {
-                    storage.add(line);
-                }
-                int index = 0;
-                if (tailLines > storage.size()) {
-                    index = 0;
-                } else {
-                    index = storage.size() - tailLines;
-                }
-                for (int i = index; i < storage.size(); i++) {
-                    output.write(storage.get(i) + System.getProperty("line.separator"));
-                    output.flush();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("tail: cannot open " + tailArg);
+        } 
+
+        if(args.size() == 0 || args.size() == 2){
+            if(args.size() == 0){
+                tailLines = 10;
             }
-        } else {
-            throw new RuntimeException("tail: " + tailArg + " does not exist");
+            else{
+                tailLines = get_tailLines(args.get(1));
+            }
+            BufferedReader reader = input;
+            write(reader,output,tailLines);
+        }else{
+            File tailFile = new File(currentDirectory + File.separator + tailArg);
+            if (tailFile.exists()) {
+                Charset encoding = StandardCharsets.UTF_8;
+                Path filePath = Paths.get((String) currentDirectory + File.separator + tailArg);
+                try (BufferedReader reader = Files.newBufferedReader(filePath, encoding)) {
+                    write(reader, output, tailLines);
+                } catch (IOException e) {
+                    throw new RuntimeException("tail: cannot open " + tailArg);
+                }
+            } else {
+                throw new RuntimeException("tail: " + tailArg + " does not exist");
+            }
         }
-    };
+    }
+
+    private void write(BufferedReader reader,OutputStreamWriter output,int tailLines) throws IOException {
+        ArrayList<String> storage = new ArrayList<>();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            storage.add(line);
+        }
+        int index = 0;
+        if (tailLines > storage.size()) {
+            index = 0;
+        } else {
+            index = storage.size() - tailLines;
+        }
+        for (int i = index; i < storage.size(); i++) {
+            output.write(storage.get(i) + System.getProperty("line.separator"));
+            output.flush();
+        }
+    }
+
+    private int get_tailLines(String arg){
+        try {
+            int x =  Integer.parseInt(arg);
+            if(x>0){
+                return x;
+            }else{
+                throw new RuntimeException("tail: illegal line count " + arg);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("tail: wrong argument " + arg);
+        }
+    }
 }
