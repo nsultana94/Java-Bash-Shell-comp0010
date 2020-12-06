6f392fe734b3fc74303db27d04a3ca57c57b593a
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2020-12-06 14:17:12 +0000

    echo comment

--- src/main/java/uk/ac/ucl/jsh/echo.java
+++ src/main/java/uk/ac/ucl/jsh/echo.java
@@ -6,10 +6,20 @@ import java.io.OutputStreamWriter;
 import java.util.List;
 
 public class echo implements Application {
+    /**
+     * The Echo Application in JSH.
+     * Takes a input string and writes it to the output
+     */
 
     public echo() throws IOException {
 	}
-
+    /**
+     * Takes a string input from args and writes it to the output
+     * @param args The command line inputs to be passed to echo
+     * @param input the standard input. Not used for echo
+     * @param output standard output to be written to
+     * @throws IOException if cannot write to output
+     */
     public void exec(List<String> args, BufferedReader input, OutputStreamWriter output) throws IOException{
         boolean atLeastOnePrinted = false;
         for (String arg : args) {
