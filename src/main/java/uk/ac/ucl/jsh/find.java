1e10af4e378faea779c8cac3aa701b964702b280
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2020-12-13 19:29:43 +0000

    adapting find to globbing, but prints twice right now

--- src/main/java/uk/ac/ucl/jsh/find.java
+++ src/main/java/uk/ac/ucl/jsh/find.java
@@ -9,39 +9,44 @@ import java.util.regex.Pattern;
 import java.util.ArrayList;
 
 /**
- * Class implementing the JSH Application find.
- * Takes a pattern of file to match and recursivly looks for files matching that pattern
+ * Class implementing the JSH Application find. Takes a pattern of file to match
+ * and recursivly looks for files matching that pattern
+ * 
  * @author Saachi Pahwa
- * @author Naima Sultana 
+ * @author Naima Sultana
  * @author Joshua Mukherjee
  */
 
 public class find implements Application {
-    
+
     /**
      * Method to recursivly find files matching a pattern
+     * 
      * @param targetDirectory directory to look in
-     * @param patternArg Pattern to match to locate files
-     * @param returnPath Accumulator for path followed
-     * @param returnPaths Accumulator for files found
+     * @param patternArg      Patterns to match to locate files
+     * @param returnPath      Accumulator for path followed
+     * @param returnPaths     Accumulator for files found
      * @return {@code ArrayList} of files found
      */
 
     public ArrayList<String> findFile(File targetDirectory, String patternArg, String returnPath,
-            ArrayList<String> returnPaths) {
+            ArrayList<String> argReturnPaths) {
         File[] targetDirListing = targetDirectory.listFiles();
         if (targetDirListing != null) {
             for (File child : targetDirListing) {
                 if (Pattern.matches(patternArg, child.getName())) {
-                    returnPaths.add(returnPath + "/" + targetDirectory.getName() + "/" + child.getName());
+                    argReturnPaths.add(returnPath + "/" + targetDirectory.getName() + "/" + child.getName());
 
                 }
-                if(returnPath.compareTo("") == 0){findFile(child, patternArg, targetDirectory.getName(), returnPaths);}
-                else{findFile(child, patternArg, returnPath + "/" + targetDirectory.getName(), returnPaths);}
+                if (returnPath.compareTo("") == 0) {
+                    findFile(child, patternArg, targetDirectory.getName(), argReturnPaths);
+                } else {
+                    findFile(child, patternArg, returnPath + "/" + targetDirectory.getName(), argReturnPaths);
+                }
             }
         }
 
-        return returnPaths;
+        return argReturnPaths;
     }
 
     public find() throws IOException {
@@ -49,52 +54,79 @@ public class find implements Application {
 
     /**
      * Method to run the Find Application
-     * @param args the arguments to be passed into the app
-     * @param input {@code BufferedReader} the standard input for the app
+     * 
+     * @param args   the arguments to be passed into the app
+     * @param input  {@code BufferedReader} the standard input for the app
      * @param output {@code OutputStreamWriter} the standard output for the app
      * @throws IOException
      */
+
     @Override
     public void exec(List<String> args, BufferedReader input, OutputStreamWriter output) throws IOException {
-        String currentDirectory = directory.getCurrentDirectory();
-
-        if (args.isEmpty()) {
+        // String currentDirectory = directory.getCurrentDirectory();
+        if (args.isEmpty() || args.size() == 1) {
             throw new RuntimeException("find: missing arguments");
-        } else if (!(args.size() == 3 || args.size() == 2)) {
-            throw new RuntimeException("find: wrong arguments");
-        } else if ((args.size() == 2 && !args.get(0).equals("-name"))) {
-            throw new RuntimeException("find: wrong arguments " + args.get(0));
-        } else if ((args.size() == 3 && !args.get(1).equals("-name"))) {
-            throw new RuntimeException("find: wrong arguments " + args.get(1));
+        } else if (args.size() == 2 && args.get(1).equals("-name")) {
+            throw new RuntimeException("find: missing arguments");
+        } else if (!(args.get(0).equals("-name")) && !(args.get(1).equals("-name"))) {
+            throw new RuntimeException("find: missing argument -name");
         }
 
-        File targetDirectory;
-        String patternArg;
+        else if (args.get(1).equals("-name")) {
+            try {
+                File firstArg = new File(args.get(1));
+                firstArg.getCanonicalPath();
+            } catch (IOException e) {
+                throw new RuntimeException("find: directory path name is invalid");
+            }
+        }
 
-        if (args.size() == 2) {
+        /*
+         * else if (!(args.size() == 3 || args.size() == 2)) { throw new
+         * RuntimeException("find: wrong arguments"); } else if ((args.size() == 2 &&
+         * !args.get(0).equals("-name"))) { throw new
+         * RuntimeException("find: wrong arguments " + args.get(0)); } else if
+         * ((args.size() == 3 && !args.get(1).equals("-name"))) { throw new
+         * RuntimeException("find: wrong arguments " + args.get(1)); }
+         */
+        // -name testfile.txt textfile2.txt
+
+        File targetDirectory;
+        ArrayList<String> patternArgs = new ArrayList<String>();
+
+        if (args.get(0).equals("-name")) {
             targetDirectory = new File(".");
-            patternArg = args.get(1);
+            for (int i = 1; i <= args.size() - 1; i++) {
+                patternArgs.add(args.get(i));
+            }
         } else {
             String pathArg = args.get(0);
             targetDirectory = new File(pathArg);
             if (!targetDirectory.exists()) {
                 throw new RuntimeException("find: path does not exist " + args.get(0));
             }
-            patternArg = args.get(2);
+            for (int i = 2; i <= args.size() - 1; i++) {
+                patternArgs.add(args.get(i));
+            }
         }
 
         if (targetDirectory.listFiles() == null) {
             throw new RuntimeException("find: directory is empty");
         }
-        ArrayList<String> emptyPath = new ArrayList<>();
+        ArrayList<String> emptyPath = new ArrayList<String>();
 
-        ArrayList<String> returnPaths = findFile(targetDirectory, patternArg, "", emptyPath);
+        ArrayList<ArrayList<String>> allReturnPaths = new ArrayList<ArrayList<String>>();
 
-        for (String path : returnPaths) {
-            output.write(path);
-            output.write(System.getProperty("line.separator"));
-            output.flush();
+        for (String patternArg : patternArgs) {
+            allReturnPaths.add(findFile(targetDirectory, patternArg, "", emptyPath));
         }
 
+        for (ArrayList<String> argReturnPaths : allReturnPaths) {
+            for (String path : argReturnPaths) {
+                output.write(path);
+                output.write(System.getProperty("line.separator"));
+                output.flush();
+            }
+        }
     }
 }
