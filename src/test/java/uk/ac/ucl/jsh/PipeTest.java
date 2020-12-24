1ef8f93543885286dd114b72c8b4cc5b339277b5
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2020-12-24 11:16:26 +0000

    More pipe tests

---
+++ src/test/java/uk/ac/ucl/jsh/PipeTest.java
@@ -0,0 +1,90 @@
+package uk.ac.ucl.jsh;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class PipeTest {
+    PipedInputStream in;
+    PipedOutputStream out;
+    ArrayList<String> args;
+    OutputStreamWriter output;
+    BufferedReader input;
+    CurrentDirectory directory = CurrentDirectory.getInstance();
+
+
+    @Before
+    public void setup() throws Exception {
+        in = new PipedInputStream();
+        out = new PipedOutputStream(in);
+        args = new ArrayList<String>();
+        output = new OutputStreamWriter(out);
+        input = new BufferedReader(new InputStreamReader(in));
+
+    }
+
+    @Test
+    public void LsCat() throws IOException, InterruptedException {
+        String command = "ls | cat";
+        new pipe(command).eval(null, out);
+        out.close();
+
+        String result = input.lines().collect(Collectors.joining("\n"));
+
+       
+        String CurrentDirectory = directory.getCurrentDirectory();
+        File file = new File(CurrentDirectory);
+        String[] files = file.list();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < files.length; i++){
+            if(!(files[i].startsWith("."))){
+                System.out.println(files[i]);
+                sb.append(files[i]);
+                if(i!= files.length -1)
+                {
+                    sb.append("\n");
+                }
+            } 
+        }
+        assertEquals(result, sb.toString());
+    }
+
+    @Test
+    public void echoCat() throws IOException, InterruptedException {
+        String command = "echo hello | cat";
+        new pipe(command).eval(null, out);
+        out.close();
+        String result = input.lines().collect(Collectors.joining("\n"));
+        assertEquals(result, "hello ");
+    }
+
+    @Test
+    public void StartThrowsError() throws IOException, InterruptedException {
+        String command = "ls nonExistantDir | cat";
+        new pipe(command).eval(null, out);
+        out.close();
+        String result = input.lines().collect(Collectors.joining("\n"));
+        assertEquals(result, "");
+    }
+
+    @Test
+    public void SecondIgnoresFirst() throws IOException, InterruptedException {
+        String command = "pwd | echo hello";
+        new pipe(command).eval(null, out);
+        out.close();
+        String result = input.lines().collect(Collectors.joining("\n"));
+        assertEquals(result, "hello ");
+    }
+
+}
