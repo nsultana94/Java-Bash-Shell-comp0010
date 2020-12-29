23ca76cb07255d9770bd3e4d359add1474271f68
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2020-12-29 21:43:02 +0000

    trying to fix io redirection with no spaces

--- src/main/java/uk/ac/ucl/jsh/Call.java
+++ src/main/java/uk/ac/ucl/jsh/Call.java
@@ -12,65 +12,69 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 
 /**
- * Call class to represent a call to an application.
- * Handles IO redirection and String manipulation on the input
+ * Call class to represent a call to an application. Handles IO redirection and
+ * String manipulation on the input
+ * 
  * @author Saachi Pahwa
- * @author Naima Sultana 
+ * @author Naima Sultana
  * @author Joshua Mukherjee
  */
 
 public class Call extends Thread implements Command {
 
     private String rawCommand;
-    
+
     private BufferedReader input;
     private OutputStream output;
-    
-    public void run(){
+
+    public void run() {
         try {
             eval(input, output);
         } catch (Exception e) {
-            if(!(e.getMessage() == null)){
-                System.out.println("jsh:"+ e.getMessage());
+            if (!(e.getMessage() == null)) {
+                System.out.println("jsh:" + e.getMessage());
             }
             Thread.currentThread().interrupt();
         }
 
-    }    
+    }
 
     /**
      * constructor to take the raw string input from the command line
-     * @param rawC the String to be processed to be executed 
+     * 
+     * @param rawC the String to be processed to be executed
      */
-	public Call(String rawC) {
+    public Call(String rawC) {
         rawCommand = rawC;
-	}
+    }
 
     /**
      * Main method of the class to run the command
-     * @param input {@code BufferedReader} the standard input for the command to be executed
+     * 
+     * @param input  {@code BufferedReader} the standard input for the command to be
+     *               executed
      * @param output {@code OutputStreamWriter} the standard output for the command
-     * @throws IOException if files attempted to be opened cannot be 
+     * @throws IOException if files attempted to be opened cannot be
      */
-	public synchronized void eval(BufferedReader input, OutputStream output) throws IOException {
-        
-    
-        String currentDirectory = directory.getCurrentDirectory();
-        
-        //String regex = "`(.*?)`";
+    public synchronized void eval(BufferedReader input, OutputStream output) throws IOException {
 
-        //command substitution
+        String currentDirectory = directory.getCurrentDirectory();
+
+        // String regex = "`(.*?)`";
+
+        // command substitution
         Pattern pattern = Pattern.compile("`(.*?)`");
         Matcher matcher = pattern.matcher(rawCommand);
-        
-        if (matcher.find()){
-          
+
+        if (matcher.find()) {
+
             doCmdSub(input, output, matcher);
             return;
         }
 
-        // return array of args - for each arg in arg evaluate that arg and then do any other args 
-        
+        // return array of args - for each arg in arg evaluate that arg and then do any
+        // other args
+
         ArrayList<String> args = tokenizeCommand(rawCommand, output);
         ArrayList<String> args1 = tokenizeCommand(rawCommand, output);
 
@@ -78,71 +82,102 @@ public class Call extends Thread implements Command {
         File outputFile = null;
         Boolean inputFileBool = false;
         Boolean outputFileBool = false;
-        String nextArg ="";
+        String nextArg = "";
 
         // check if there are input and output redirections
         for (int i = 0; i < args.size(); i++) {
-           
+
             String arg = args.get(i);
-            
-            if(i!= args.size() - 1)
-            {
-                
+
+            if (i != args.size() - 1) {
+
                 nextArg = args.get(i + 1);
-            }
-            else{
+            } else {
                 nextArg = args.get(i);
             }
-            
+
             if (arg.equalsIgnoreCase("<")) {
+                System.out.println("input found");
+                System.out.println(nextArg + "next arg");
                 if (inputFileBool == true) {
-                    throw new RuntimeException("more than one I/O in same direction "  + nextArg);
+                    throw new RuntimeException("more than one I/O in same direction " + nextArg);
                 } else if (i == args.size() - 1) {
                     throw new RuntimeException(" no I/O file specified " + args.get(i));
-                } else if (!new File(currentDirectory + File.separator  + nextArg).exists()) {
-                    throw new RuntimeException("file does not exist "  + nextArg);
+                } else if (!new File(currentDirectory + File.separator + nextArg).exists()) {
+                    throw new RuntimeException("file does not exist " + nextArg);
                 } else {
-                    inputFile = new File(currentDirectory + File.separator  + nextArg);
+                    inputFile = new File(currentDirectory + File.separator + nextArg);
                     inputFileBool = true;
                     args1.remove(arg);
                     args1.remove(nextArg);
                 }
-            }else if (arg.equalsIgnoreCase(">")) {
+            } else if (arg.charAt(0) == '<') {
+                if (inputFileBool == true) {
+                    throw new RuntimeException("more than one I/O in same direction " + arg);
+                }
+                arg = arg.replace("<", "");
+                if (!new File(currentDirectory + File.separator + arg).exists()) {
+                    throw new RuntimeException("file does not exist " + arg);
+                } else {
+                    inputFile = new File(currentDirectory + File.separator + arg);
+                    inputFileBool = true;
+                    args1.remove(arg);
+                }
+            } else if (arg.equalsIgnoreCase(">")) {
+                System.out.println("output found");
+                System.out.println(nextArg + "next arg");
                 output.close();
                 if (outputFileBool == true) {
-                    throw new RuntimeException(" more than one I/O in same direction "  + nextArg);
+                    throw new RuntimeException(" more than one I/O in same direction " + nextArg);
                 } else if (i == args.size() - 1) {
                     throw new RuntimeException(" no I/O file specified " + args.get(i));
-                } else if (!new File(currentDirectory + File.separator  + nextArg).exists()) {
+                } else if (!new File(currentDirectory + File.separator + nextArg).exists()) {
                     outputFile = new File(currentDirectory + File.separator + nextArg);
                     outputFile.createNewFile();
                     outputFileBool = true;
                     args1.remove(arg);
                     args1.remove(nextArg);
-                } else if (new File(currentDirectory + File.separator  + nextArg).exists())
-                {
+                } else if (new File(currentDirectory + File.separator + nextArg).exists()) {
                     outputFile = new File(currentDirectory + File.separator + nextArg);
                     outputFileBool = true;
                     args1.remove(arg);
                     args1.remove(nextArg);
                 }
+            } else if (arg.charAt(0) == '>') {
+                System.out.println("output found");
+                if (outputFileBool == true) {
+                    throw new RuntimeException(" more than one I/O in same direction " + arg);
+                }
+                arg = arg.replace(">", "");
+                System.out.println(arg + "arg");
+                if (!new File(currentDirectory + File.separator + arg).exists()) {
+                    outputFile = new File(currentDirectory + File.separator + arg);
+                    outputFile.createNewFile();
+                    outputFileBool = true;
+                    args1.remove(arg);
+                } else if (new File(currentDirectory + File.separator + arg).exists()) {
+                    outputFile = new File(currentDirectory + File.separator + arg);
+                    outputFileBool = true;
+                    args1.remove(arg);
+                }
             }
         }
+
         if (outputFileBool == true) {
-    
+
             output = new FileOutputStream(outputFile);
         }
         if (inputFileBool == true) {
             input = new BufferedReader(new FileReader(inputFile));
         }
         executeCommand(args1, input, output);
-        }
-           
- 
+    }
+
     /**
-     * Takes a String command and converts this into a List of tokens. 
+     * Takes a String command and converts this into a List of tokens.
+     * 
      * @param rawCommands The raw unprocessed string to be converted into tokens
-     * @param output {@code OutputStream} the Standard output for the app
+     * @param output      {@code OutputStream} the Standard output for the app
      * @return ArrayList of tokens for the command
      * @throws IOException
      */
@@ -150,20 +185,22 @@ public class Call extends Thread implements Command {
         glob glob_processor = new glob();
         ArrayList<String> args = glob_processor.get_tokens(rawCommands);
         return args;
-    } 
+    }
 
     /**
      * Creates the application and calls exec on it
-     * @param args The Arguments to be used bt the command
-     * @param input The Standard input
+     * 
+     * @param args   The Arguments to be used bt the command
+     * @param input  The Standard input
      * @param output The standard output
      */
-    public synchronized void executeCommand(ArrayList<String> args, BufferedReader input, OutputStream output) throws IOException {
+    public synchronized void executeCommand(ArrayList<String> args, BufferedReader input, OutputStream output)
+            throws IOException {
         ApplicationFactory applicationFactory = new ApplicationFactory();
         ArrayList<String> appArgs = new ArrayList<String>();
         String appName = args.get(0);
-        Boolean unsafe = (rawCommand.charAt(0) == '_')? true: false;
-        appName = unsafe? appName.substring(1): appName;
+        Boolean unsafe = (rawCommand.charAt(0) == '_') ? true : false;
+        appName = unsafe ? appName.substring(1) : appName;
         appArgs = new ArrayList<String>(args.subList(1, args.size()));
         Application command = applicationFactory.getApplication(appName, unsafe);
         command.exec(appArgs, input, new OutputStreamWriter(output));
@@ -171,46 +208,40 @@ public class Call extends Thread implements Command {
     }
 
     /**
-    * Runs command substitution on the application
-    * @param input The Standard input
-    * @param output The standard output
-    * @param matcher The Regular Expression to find where the sub commands are
-    * @throws IOException if cannot open file
-    */
+     * Runs command substitution on the application
+     * 
+     * @param input   The Standard input
+     * @param output  The standard output
+     * @param matcher The Regular Expression to find where the sub commands are
+     * @throws IOException if cannot open file
+     */
     public synchronized void doCmdSub(BufferedReader input, OutputStream output, Matcher matcher) throws IOException {
-        
+
         ArrayList<String> cmdsubinput = new ArrayList<String>();
         String cmdsub = "";
         cmdsub = matcher.group();
         String command = cmdsub;
-       
-       
+
         cmdsub = cmdsub.replace("`", "");
-       
-        
+
         CommandSubstitution subcmd = new CommandSubstitution(cmdsub);
         cmdsubinput = subcmd.get_output(input);
         ArrayList<String> commandsubargs = new ArrayList<String>();
-        
 
-        for(String arg: cmdsubinput){
+        for (String arg : cmdsubinput) {
             String temp = rawCommand;
             temp = temp.replace(command, arg);
             commandsubargs = tokenizeCommand(temp, output);
-           
+
             executeCommand(commandsubargs, input, output);
-           
-            
-          
+
         }
-            
-        
+
     }
 
     public void setInput(BufferedReader input) {
         this.input = input;
     }
-
 
     public void setOutput(OutputStream output) {
         this.output = output;
@@ -223,8 +254,5 @@ public class Call extends Thread implements Command {
     public OutputStream getOutput() {
         return output;
     }
-    
 
 }
-
-
