724665ac310f399035ea754025fdfa97a5b0ba20
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2021-01-05 15:08:55 +0000

    Current directory

--- src/main/java/uk/ac/ucl/jsh/ApplicationFactory.java
+++ src/main/java/uk/ac/ucl/jsh/ApplicationFactory.java
@@ -3,21 +3,23 @@ package uk.ac.ucl.jsh;
 import java.io.IOException;
 
 /**
- * ApplicationFactory Class using the factory pattern to create new Applications.
- * Can return a 'safe' and 'unsafe' version
- * The 'unsafe' version prints its errors to standard output and terminates instead of throwing an error
+ * ApplicationFactory Class using the factory pattern to create new
+ * Applications. Can return a 'safe' and 'unsafe' version The 'unsafe' version
+ * prints its errors to standard output and terminates instead of throwing an
+ * error
+ * 
  * @author Saachi Pahwa
- * @author Naima Sultana 
+ * @author Naima Sultana
  * @author Joshua Mukherjee
  */
-
 
 public class ApplicationFactory {
     /**
      * Creates an application object given an application name
+     * 
      * @param appName the name of the application to be created
-     * @param unsafe {@code true} if unsafe or {@code false} if normal
-     * @return Application object optionally with a UnsafeDecorator 
+     * @param unsafe  {@code true} if unsafe or {@code false} if normal
+     * @return Application object optionally with a UnsafeDecorator
      * @see UnsafeDecorator
      * @see Application
      */
@@ -29,16 +31,13 @@ public class ApplicationFactory {
             return null;
         } else if (appName.equalsIgnoreCase("pwd")) {
             app = new Pwd();
-        }
-        else if (appName.equalsIgnoreCase("ls")) {
+        } else if (appName.equalsIgnoreCase("ls")) {
             app = new Ls();
         } else if (appName.equalsIgnoreCase("cat")) {
             app = new cat();
-        }
-        else if (appName.equalsIgnoreCase("echo")) {
+        } else if (appName.equalsIgnoreCase("echo")) {
             app = new echo();
-        }
-        else if (appName.equalsIgnoreCase("head")) {
+        } else if (appName.equalsIgnoreCase("head")) {
             app = new head();
         } else if (appName.equalsIgnoreCase("tail")) {
             app = new tail();
@@ -52,8 +51,7 @@ public class ApplicationFactory {
 
         else if (appName.equalsIgnoreCase("uniq")) {
             app = new Uniq();
-        }
-        else if(appName.equalsIgnoreCase("cut")){
+        } else if (appName.equalsIgnoreCase("cut")) {
             app = new Cut();
         }
 
